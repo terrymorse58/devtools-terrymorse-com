9adfe4a2cbdeca215221a9d575cf954cfe516332
Terry Morse <tmorse@terrymorse.com>
2021-11-23 10:15:34 -0800

    update index.html to use jsdelivr for imgs

--- gulp-src.js
+++ gulp-src.js
@@ -1,5 +1,8 @@
 const { series, src, dest } = require('gulp');
-// const replace = require('gulp-replace');
+const replace = require('gulp-replace');
+const touch = require('gulp-touch-fd');
+
+const CDN_IMGS = 'https://cdn.jsdelivr.net/gh/terrymorse58/devtools-terrymorse-com/src/imgs/';
 
 const SRC = './src/';
 const DIST = 'dist/';
@@ -9,4 +12,14 @@ function copySrc () {
     .pipe(dest(DIST));
 }
 
-exports.default = series(copySrc);
+function copyIndex () {
+  return src(SRC + 'index.html')
+    .pipe(replace(
+      /"imgs\//g,
+      `"${CDN_IMGS}`
+    ))
+    .pipe(dest(DIST))
+    .pipe(touch());
+}
+
+exports.default = series(copySrc, copyIndex);
